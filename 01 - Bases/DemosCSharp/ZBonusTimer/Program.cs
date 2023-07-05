4de2923adf70e482cfac07583587625533e0698c
Guillaume Mairesse <guillaume@utopios.solutions>
2023-07-05 12:21:12 +0200

    correction 13 + bonus timer

---
+++ 01 - Bases/DemosCSharp/ZBonusTimer/Program.cs
@@ -0,0 +1,14 @@
+﻿DateTime fin = new DateTime(2023, 07, DateTime.Now.Day, 12, 15, 0);
+var delta = fin - DateTime.Now;
+Console.ForegroundColor = ConsoleColor.DarkRed;
+
+while (delta.TotalSeconds > 0)
+{
+    delta = fin - DateTime.Now;
+    Console.WriteLine(delta.ToString(@"hh\:mm\:ss"));
+    Thread.Sleep(500);
+    Console.Clear();
+}
+
+Console.WriteLine("TEMPS ECOULÉ !");
+Console.ReadKey();
