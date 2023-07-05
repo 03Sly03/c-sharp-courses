250da1b0f850ec2fb29e3c88a8a9334fda6b7a0f
Guillaume Mairesse <guillaume@utopios.solutions>
2023-07-05 15:34:42 +0200

    correction 17 18

--- 01 - Bases/DemosCSharp/DemosCSharp/Program.cs
+++ 01 - Bases/DemosCSharp/DemosCSharp/Program.cs
@@ -1,1 +1,9 @@
 ﻿
+//string? variable = "du texte";
+string? variable = null;
+
+string ageString = variable ?? "saisie vide !";
+string ageString2 = variable == null ? "saisie vide !" : variable;
+
+Console.WriteLine(ageString);
+Console.WriteLine(ageString2);
