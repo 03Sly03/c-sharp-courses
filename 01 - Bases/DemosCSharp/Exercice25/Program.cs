ea9a643a561b6261ce5e89282280aa5d3afb8995
Guillaume Mairesse <guillaume@utopios.solutions>
2023-07-06 11:45:20 +0200

    corrections for

---
+++ 01 - Bases/DemosCSharp/Exercice25/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("--- Gestion des notes ---\n\n");
+Console.WriteLine("Veuillez saisir 5 notes : \n\n");
+float max = 0, min = 20, total = 0, moyenne;
+for (int i = 1; i <= 5; i++)
+{
+    Console.Write("\t - Merci de saisir la note " + i + "(sur /20) :");
+    int note = int.Parse(Console.ReadLine());
+    if (note > max)
+        max = note;
+    if (note < min)
+        min = note;
+    total += note;
+}
+moyenne = total / 5;
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("\nla meilleure note est " + max + "/20");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("la moins bonne note est " + min + "/20");
+Console.ForegroundColor = ConsoleColor.Gray;
+Console.WriteLine("la moyenne des note est " + moyenne + "/20");
--- 01 - Bases/DemosCSharp/ZBonusTimer/Program.cs
+++ 01 - Bases/DemosCSharp/ZBonusTimer/Program.cs
@@ -1,4 +1,4 @@
-﻿DateTime fin = new DateTime(2023, 07, DateTime.Now.Day, 15, 0, 0);
+﻿DateTime fin = new DateTime(2023, 07, DateTime.Now.Day, 10, 45, 0);
 var delta = fin - DateTime.Now;
 Console.ForegroundColor = ConsoleColor.DarkRed;
 
