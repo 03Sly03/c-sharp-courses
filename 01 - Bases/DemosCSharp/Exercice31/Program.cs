a2c3c5d4cdc5996ea756d04571242fa1d4da6f7a
Guillaume Mairesse <guillaume@utopios.solutions>
2023-07-07 09:25:19 +0200

    correction 25 - 30

---
+++ 01 - Bases/DemosCSharp/Exercice31/Program.cs
@@ -0,0 +1,37 @@
+﻿//définir min max sum nbnotes
+
+while (true)
+{
+    string choice;
+    Console.WriteLine("--- Gestion des notes avec menu ---\n ");
+    Console.Write("1----Saisir les notes" +
+        "\n2----La meilleure note" +
+        "\n3----La pire note" +
+        "\n4----La moyenne des notes" +
+        "\n0----Quitter" +
+        "\n\nFaites votre choix : ");
+    choice = Console.ReadLine();
+    Console.Clear();
+
+    switch (choice)
+    {
+        case "1":
+            Console.Write("cas 1");
+            break;
+        case "2":
+            Console.Write("cas 2");
+            break;
+        case "3":
+            Console.Write("cas 3");
+            break;
+        case "4":
+            Console.Write("cas 4");
+            break;
+        case "0":
+            Console.Write("cas 0"); // sortir du programme
+            break;
+        default:
+            Console.WriteLine("Erreur de saisie, recommencez !");
+            break;
+    }
+}
