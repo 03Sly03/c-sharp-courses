f660fa03f8e37b376869d6adba07965ce7b2b01f
Guillaume Mairesse <guillaume@utopios.solutions>
2023-07-18 16:28:37 +0200

    operateurs is et as

--- 02 - POO/CSharpPOO/Demo06HeritageOverrideAbstract/Program.cs
+++ 02 - POO/CSharpPOO/Demo06HeritageOverrideAbstract/Program.cs
@@ -31,13 +31,43 @@ Animal chat = new Chat("Mistigri", true, "femelle", false);
 //chat.Allaiter();
 
 
-List<Animal> animals = new List<Animal>
+List<object> animals = new List<object>
 {
+    1,
+    "test",
     chat,
     chien
 };
 
-foreach (var ani in animals)
+// operateur is
+foreach (object mon_objet in animals)
 {
-    ani.Crier();
+    //ani.Crier();
+
+    if (mon_objet is Animal mon_animal) // on teste si c'est un animal et on l'envoie si c'est le cas dans la variable mon_animal
+    {
+        Console.WriteLine("C'est un animal !");
+        if (mon_objet is Chat mon_chat)
+        {
+            Console.WriteLine("C'est un chat !");
+            mon_chat.Allaiter();
+        }
+        if (mon_animal is Chien)
+        {
+            Console.WriteLine("C'est un chien !");
+        }
+        mon_animal.Crier();
+    }
+    else
+        Console.WriteLine("Ce n'est pas un animal !");
+
+    Console.WriteLine("-----------------------------------------");
+}
+
+
+// operateur as
+foreach (object mon_objet in animals)
+{
+    Chat? mon_chat = mon_objet as Chat;
+    Console.WriteLine("Contenu de la variable mon_chat (vide si null) : " + mon_chat);
 }
