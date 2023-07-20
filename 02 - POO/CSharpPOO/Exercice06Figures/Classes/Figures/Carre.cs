34190a9dcd690894b7c279d46f7d9fd2f9c17213
Guillaume Mairesse <guillaume@utopios.solutions>
2023-07-20 10:37:27 +0200

    debut correction figure

---
+++ 02 - POO/CSharpPOO/Exercice06Figures/Classes/Figures/Carre.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice06Figures.Classes.Figures
+{
+    internal class Carre : Figure
+    {
+        /*         +cote
+         *        A    B
+         *  -cote
+         *        D    c
+         */
+
+        public double Cote { get; set; }
+
+        public Point B => new Point(A.X + Cote, A.Y);
+
+        public Carre(Point a, double cote) : base(a)
+        {
+            Cote = cote;
+        }
+
+        public Carre(double x, double y, double cote) : base(x, y)
+        {
+            Cote = cote;
+        }
+    }
+}
---
+++ 02 - POO/CSharpPOO/Exercice06Figures/Classes/Figures/Rectangle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice06Figures.Classes.Figures
+{
+    internal class Rectangle
+    {
+    }
+}
