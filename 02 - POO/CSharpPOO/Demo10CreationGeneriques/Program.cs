4f275ec7a98722b6fe0e779fa5d81260441c3ee5
Guillaume Mairesse <guillaume@utopios.solutions>
2023-07-20 15:12:03 +0200

    enum + collections + généricité

---
+++ 02 - POO/CSharpPOO/Demo10CreationGeneriques/Program.cs
@@ -0,0 +1,22 @@
+﻿using Demo10CreationGeneriques.Classes;
+
+////Demarreur<List<string>> t = new Demarreur<List<string>>();
+////Demarreur<Queue<string>> t2 = new Demarreur<Queue<string>>();
+////Demarreur<string[]> t3 = new Demarreur<string[]>();
+
+//Demarreur<Ordinateur> demarreurDOrdinateur = new Demarreur<Ordinateur>();
+
+//demarreurDOrdinateur.DemarreCetteChose(new Ordinateur());
+////demarreurDOrdinateur.DemarreCetteChose(new Tronconeuse());
+
+
+//Arreteur arreteur = new Arreteur();
+
+//arreteur.ArreteCetteChose<Tronconeuse>(new Tronconeuse());
+//arreteur.ArreteCetteChose(new Ordinateur());
+
+
+Scribe.EcritPlusieursFois("chaine",2);
+Scribe.EcritPlusieursFois(1,5);
+Scribe.EcritPlusieursFois(1.0,3);
+Scribe.EcritPlusieursFois(new Tronconeuse(),1);
