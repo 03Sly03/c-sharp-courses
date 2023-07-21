f6bb9e5f898a8f47ecc716e610329dcfea14a1f4
Guillaume Mairesse <guillaume@utopios.solutions>
2023-07-21 12:30:05 +0200

    demo delegates

---
+++ 02 - POO/CSharpPOO/Demo11Delegate/Calculatrice.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo11Delegate
+{
+    internal class Calculatrice
+    {
+        public int Addition(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Soustraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        public void CalculerEtAfficher(int a, int b, Func<int, int, int> methodeDeCalcul)
+        {
+            int resultat = methodeDeCalcul(a, b);
+            Console.WriteLine($"L'operation de calcul entre les entiers {a} et {b} donne {resultat}");
+        }
+    }
+}
---
+++ 02 - POO/CSharpPOO/Demo11Delegate/Program.cs
@@ -0,0 +1,57 @@
+﻿//using Demo11Delegate;
+
+// fonctions locales
+using Demo11Delegate;
+
+int Carre(int a)
+{
+    return a * a;
+}
+int Multiplication(int a, int b)
+{
+    return a * b;
+}
+
+
+Console.WriteLine(Carre);
+Console.WriteLine(Carre(2));
+Console.WriteLine(Multiplication(2,5));
+
+// Type Func 
+Func<int, int> carre = Carre;
+Func<int, int> carre2 = a => a * a; // une lambda expression aussi appelée fonction annonyme 
+Func<int, int> carre3 = (int a) => a * a; 
+
+Console.WriteLine(carre);
+Console.WriteLine(carre(2));
+Console.WriteLine(carre2(2));
+
+Func<int, int, int> mul = Multiplication;
+Func<int, int, int> mul2 = (a, b) => a * b;
+Func<int, int, int> mul3 = (int a, int b) => a * b; // une seule instruction => lambda possible
+Func<int, int, int> mul4 = delegate (int a, int b) // une ou plusieurs instructions => delegate (bloc d'instruction {} obligatoire
+{
+    Console.WriteLine($"Je multiplie {a} et {b}.");
+    return a * b; 
+}; 
+
+Console.WriteLine(mul);
+Console.WriteLine(mul(2, 5));
+Console.WriteLine(mul2(2, 6));
+Console.WriteLine(mul4(2, 6));
+
+
+Console.WriteLine("-----------------------------------");
+
+Calculatrice cal = new Calculatrice();
+
+var add = cal.Addition; // Func
+Console.WriteLine(add(1,2));
+
+cal.CalculerEtAfficher(10, 20, Multiplication);
+cal.CalculerEtAfficher(10, 20, cal.Addition);
+cal.CalculerEtAfficher(10, 20, add);
+cal.CalculerEtAfficher(10, 20, mul4);
+cal.CalculerEtAfficher(10, 20, (a,b) => a % b);
+cal.CalculerEtAfficher(30, 10, (a,b) => a / b);
+cal.CalculerEtAfficher(30, 10, delegate (int a, int b) { return a / b; });
