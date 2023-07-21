abb00d4ae9511711665d734fccd1173f90c7e867
Guillaume Mairesse <guillaume@utopios.solutions>
2023-07-21 09:44:48 +0200

    début pile + support poo général

---
+++ 02 - POO/CSharpPOO/Exercice07Pile/Classes/Pile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice07Pile.Classes
+{
+    internal class Pile<T>
+    {
+        private List<T> _values = new List<T>();
+
+        public void Empiler(T element)
+        {
+
+        }
+    }
+}
